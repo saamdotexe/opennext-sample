a6b1c4c14e22832a70365cabf0a43ed9e205eda0
saamdotexe <djhh9991@gmail.com>
2025-02-06 01:12:06 +0100

    migrated @opennextjs/cloudflare from 0.3.10 to 0.4.5

--- src/db/dz.ts
+++ src/db/dz.ts
@@ -2,7 +2,7 @@ import { drizzle, DrizzleD1Database } from 'drizzle-orm/d1';
 import { getCloudflareContext } from "@opennextjs/cloudflare";
 
 async function dz(): Promise<DrizzleD1Database> {
-    let ctx = await getCloudflareContext()
+    let ctx = getCloudflareContext()
     const db = drizzle(ctx.env.D1)
     return db
 }
